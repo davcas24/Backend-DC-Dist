4a891d5d3d1430079bdfee677d86839e7e0ec299
davcas24 <davidacastro.24@gmail.com>
2015-12-09 09:30:58 -0600

    adding baseurl

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ var authController = require ('./controllers/authController');
 
 
 
-exports.endpoints = [{method: 'GET', path: '/{param*}', config: {handler: function(request, reply){reply('API v1, Students')}}},
+exports.endpoints = [{method: 'GET', path: '/{param*}', config: {handler: function(request, reply){reply('API v1, Users')}}},
 					 {method: 'GET', path: '/v1/usuarios', config: usuarioController.getusuario},
            {method: 'POST', path: '/usuario', config: usuarioController.createusuario},
 					 {method: 'POST', path: '/login', config: authController.login},
